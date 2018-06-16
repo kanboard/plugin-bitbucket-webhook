60b25e7909b9b5c7bf76031c2e48beee7981979c
Balázs Úr <urbalazs@gmail.com>
2018-06-16 17:45:36 +0200

    Updated Hungarian translation

--- Locale/hu_HU/translations.php
+++ Locale/hu_HU/translations.php
@@ -1,15 +1,15 @@
 <?php
 
 return array(
-    'Bitbucket commit received' => 'Bitbucket commit érkezett',
-    'Bitbucket webhooks' => 'Bitbucket webhooks',
-    'Help on Bitbucket webhooks' => 'Bitbucket webhooks súgó',
-    // 'By @%s on Bitbucket' => '',
-    // 'Bitbucket Issue' => '',
-    // 'Commit made by @%s on Bitbucket' => '',
-    // 'Bitbucket issue opened' => '',
-    // 'Bitbucket issue closed' => '',
-    // 'Bitbucket issue reopened' => '',
-    // 'Bitbucket issue assignee change' => '',
+    'Bitbucket commit received' => 'Bitbucket kommit érkezett',
+    'Bitbucket webhooks' => 'Bitbucket webhorgok',
+    'Help on Bitbucket webhooks' => 'Súgó a Bitbucket webhorgokhoz',
+    'By @%s on Bitbucket' => '@%s által a Bitbucketen',
+    'Bitbucket Issue' => 'Bitbucket probléma',
+    'Commit made by @%s on Bitbucket' => '@%s által készített kommit a Bitbucketen',
+    'Bitbucket issue opened' => 'Bitbucket probléma megnyitva',
+    'Bitbucket issue closed' => 'Bitbucket probléma lezárva',
+    'Bitbucket issue reopened' => 'Bitbucket probléma újranyitva',
+    'Bitbucket issue assignee change' => 'Bitbucket problémához hozzárendelt személy megváltoztatása',
 );
 
